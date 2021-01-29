2a8afac3a65e868587713513ee354800f9a1510c
Islam Qelash <islam2ela4@gmail.com>
2021-01-29 03:33:28 +0200

    second commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:firbase/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'file:///C:/Users/infinitu/AndroidStudioProjects/flutterProjects/firbase/lib/ui/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
